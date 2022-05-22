2e72535fb40448ab0ca679ff59fbfa421561e34a
Otso Björklund <otso.bjorklund@gmail.com>
2022-05-22 12:39:01 +0300

    Use version number in kernel name

--- script/install_kernel.sh
+++ script/install_kernel.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 poetry install -vv
 pyb -vv && pip install -e target/dist/musii_kit-*
-python -m ipykernel install --user --name=musii-kit
+python -m ipykernel install --user --name=musii-kit-0.1.0
--- script/update_kernel.sh
+++ script/update_kernel.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 pyb -vv && pip install -e target/dist/musii_kit-*
-python -m ipykernel install --user --name=musii-kit
+python -m ipykernel install --user --name=musii-kit-0.1.0
