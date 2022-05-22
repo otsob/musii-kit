e472e2db737c80959afdbc62042718eac31f4171
Otso Björklund <otso.bjorklund@gmail.com>
2022-05-22 12:52:20 +0300

    Add project description fields

--- build.py
+++ build.py
@@ -1,14 +1,15 @@
 #   -*- coding: utf-8 -*-
-from pybuilder.core import use_plugin, init, task
+from pybuilder.core import use_plugin, init
 
 use_plugin("python.core")
 use_plugin("python.unittest")
 use_plugin("python.flake8")
 use_plugin("python.distutils")
 
-
 name = "musii_kit"
 default_task = "publish"
+description = "Musicology utilities"
+long_description = "A collection of Python utilities for computational musicology."
 
 
 @init
