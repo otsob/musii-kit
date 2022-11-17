1eeec63cc9e95bda27cb964162174dcf9bc32dd0
Otso Björklund <otso.bjorklund@gmail.com>
2022-11-17 19:22:54 +0200

    Improve visualizer
    - Improve documentation
    - Make Lilypond visualizer work (although it's not great)

--- musii_kit/score_visualizer/visualizer.py
+++ musii_kit/score_visualizer/visualizer.py
@@ -1,11 +1,16 @@
+import os
+
 import music21 as m21
 from music21.environment import UserSettingsException
-import os
+
+# The visualizer is set by modifying music21 global settings, therefore
+# this unfortunately is implemented as modification of global state.
+VISUALIZER = ''
 
 
 def set_muse_score_visualizer(mscore_path=os.getenv('MUSE_SCORE_PATH')):
     """
-    Sets the path for using MuseScore as a visualizer.
+    Sets MuseScore globally as the visualizer for music notation.
 
     :param mscore_path: the path to the MuseScore executable.
                         Defaults to the value of the environment variable MUSE_SCORE_PATH.
@@ -17,6 +22,9 @@ def set_muse_score_visualizer(mscore_path=os.getenv('MUSE_SCORE_PATH')):
     us = __get_m21_user_settings()
     us['musescoreDirectPNGPath'] = mscore_path
     us['musicxmlPath'] = mscore_path
+
+    global VISUALIZER
+    VISUALIZER = 'musescore'
 
 
 def __get_m21_user_settings():
@@ -31,16 +39,31 @@ def __get_m21_user_settings():
 
 def set_lilypond_visualizer(lilypond_path=os.getenv('LILYPOND_PATH')):
     """
-    Sets the path for using LilyPond as a visualizer.
+    Sets Lilypond globally as the visualizer for music notation.
+    It is preferred to use the MuseScore visualizer as it produces better results.
 
     :param lilypond_path: the path to the LilyPond executable.
                             Defaults to the value of the environment variable LILYPOND_PATH.
     """
     us = __get_m21_user_settings()
     us['lilypondPath'] = lilypond_path
-    us['musicxmlPath'] = lilypond_path
+    us['lilypondFormat'] = 'png'
+
+    global VISUALIZER
+    VISUALIZER = 'lilypond'
 
 
 def visualize(notation):
-    """ Visualizes the given music21 notation with the visualization backend that has been set. """
-    notation.show('ipython.musicxml.png')
+    """
+    Visualizes the given music21 notation with the visualization backend that has been set.
+    Requires visualizer to have been set with either `set_muse_score_visualizer` (preferred) or
+    `set_lilypond_visualizer`.
+
+    :param notation: a music21 notation type that supports visualization """
+    global VISUALIZER
+    if VISUALIZER == 'musescore':
+        notation.show('ipython.musicxml.png')
+    elif VISUALIZER == 'lilypond':
+        notation.show('ipython.lily.png')
+    else:
+        raise ValueError('No visualizer set')
