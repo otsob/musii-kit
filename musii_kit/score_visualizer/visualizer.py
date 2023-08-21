e29817ea7425c259714b3d89abf214455cd30eb3
Otso Björklund <otso.bjorklund@gmail.com>
2023-08-21 12:00:41 +0300

    Enable visualizing notation with verovio

--- musii_kit/score_visualizer/visualizer.py
+++ musii_kit/score_visualizer/visualizer.py
@@ -1,6 +1,10 @@
 import os
+import tempfile
+from copy import deepcopy
 
 import music21 as m21
+import verovio
+from IPython.display import SVG, display
 from music21.environment import UserSettingsException
 
 # The visualizer is set by modifying music21 global settings, therefore
@@ -53,11 +57,78 @@ def set_lilypond_visualizer(lilypond_path=os.getenv('LILYPOND_PATH')):
     VISUALIZER = 'lilypond'
 
 
+def default_verovio_options():
+    return {'breaks': 'encoded',
+            'scale': 40,
+            'condenseFirstPage': True,
+            'adjustPageHeight': True,
+            'adjustPageWidth': False,
+            'header': 'auto',
+            'footer': 'none',
+            'condense': 'encoded'}
+
+
+VEROVIO_OPTIONS = default_verovio_options()
+
+
+def set_verovio_visualizer(options=default_verovio_options()):
+    """
+    Sets verovio as the visualizer to use.
+
+    Verovio is the default option as it does not require having additional
+    software installed
+
+    :param options: the options passed to the verovio renderer
+    """
+    global VEROVIO_OPTIONS
+    VEROVIO_OPTIONS = options
+
+    global VISUALIZER
+    VISUALIZER = 'verovio'
+
+
+def __clean_up_credits(notation):
+    """ Removes the text boxes for credits from the music21 stream if present.
+
+    This is needed because the presence of the TextBox credits will lead to no
+    title being rendered in the notation with verovio.
+    """
+    text_boxes = notation.getElementsByClass('TextBox')
+
+    if not text_boxes:
+        return notation
+
+    notation = deepcopy(notation)
+
+    while text_boxes := notation.getElementsByClass('TextBox'):
+        i = notation.index(text_boxes[0])
+        notation.pop(i)
+
+    return notation
+
+
+def __visualize_with_verovio(notation, options):
+    with tempfile.NamedTemporaryFile(suffix='.musicxml') as tmp:
+        name = tmp.name
+        cleaned_up_notation = __clean_up_credits(notation)
+
+        cleaned_up_notation.write('musicxml', fp=name)
+
+        tk = verovio.toolkit()
+        tk.loadFile(name)
+        tk.setOptions(options)
+        tk.redoLayout()
+        page_count = tk.getPageCount()
+
+        for i in range(1, page_count + 1):
+            display(SVG(tk.renderToSVG(i)))
+
+
 def visualize(notation):
     """
     Visualizes the given music21 notation with the visualization backend that has been set.
-    Requires visualizer to have been set with either `set_muse_score_visualizer` (preferred) or
-    `set_lilypond_visualizer`.
+    Uses verovio by default. For other visualizers they need to have been set with either
+    `set_muse_score_visualizer` or `set_lilypond_visualizer`.
 
     :param notation: a music21 notation type that supports visualization """
     global VISUALIZER
@@ -66,4 +137,5 @@ def visualize(notation):
     elif VISUALIZER == 'lilypond':
         notation.show('ipython.lily.png')
     else:
-        raise ValueError('No visualizer set')
+        global VEROVIO_OPTIONS
+        __visualize_with_verovio(notation, VEROVIO_OPTIONS)
