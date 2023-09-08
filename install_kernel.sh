349eb7c3206a1335758efe42e9e605af2d24c954
Otso Björklund <otso.bjorklund@gmail.com>
2023-09-08 17:21:14 +0300

    Remove posemirpy from muusi-kit
    Use from the posemir package instead

--- install_kernel.sh
+++ install_kernel.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
-
-cd posemirpy
-poetry install -vv --no-interaction --with dev $1
-maturin develop -r
-cd ..
-
 poetry install -vv --no-interaction --without dev $1
 python -m ipykernel install --user --name=musii-kit-0.4
--- posemirpy/posemirpy/posemirpy.pyi
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def run_siatec_c(np_points_array: np.ndarray, max_ioi: float):
-    ...
-
-
-def find_occurrences(query_points_array: np.ndarray, np_points_array: np.ndarray, min_match_size: int):
-    ...
